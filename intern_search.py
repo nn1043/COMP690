2e61bf0d1069ed591322403bde2ebabda66ddcb1
nn1043 <55171942+nn1043@users.noreply.github.com>
2020-09-19 15:29:44 -0400

    Add files via upload
    cleaned up prep_csv()

--- intern_search.py
+++ intern_search.py
@@ -27,27 +27,44 @@ def print_menu():
     print("Prep Test CSV: PREP")
     print("Test Prepped CSV: TEST")
 
+
 def prep_csv(filename):
     print("Prepping test CSV...")
     last_name = ""
-    survey = open(filename, "r")
+    first_name = ""
+    student_id = ""
+    survey = open(filename, "r", encoding = "ISO-8859-1")
     for line in survey.readlines():
         survey_readable = line.split(",")
         for value in survey_readable:
             if survey_readable.index(value) == 0:
                 last_name = value
-                intern_list[last_name] = []
-# for new csv test initial dict against 'standard' write function for
-# acceptable difference in spelling, or create new column. Maybe check for
-# 'if unique, compare char, then add new column'
-            else
-                intern_list[last_name].append(value)
+            elif survey_readable.index(value) == 1:
+                first_name = value
+            elif survey_readable.index(value) == 2: # use student ID for unique
+                student_id = value
+                intern_list[student_id] = [last_name, first_name]
+            elif value == "":
+                if (check_last == True) & (len(intern_list[student_id]) >= 24):
+                    break
+                    #intern_list[student_id].pop() # <- find way to remove last
+                else:
+                    value = "N/A"
+                    intern_list[student_id].append(value)
+                    check_last = True
+            else:
+                intern_list[student_id].append(value)
+                check_last = False
     survey.close()
     print("Test CSV loaded.")
 
+# for new csv test initial dict against 'standard' write function for
+# acceptable difference in spelling, or create new column. Maybe check for
+# 'if unique, compare char, then add new column'
+
 
 def test_prepped_CSV():
-    print(intern_list["George"])
+    print(intern_list["913035402"])
 
 
 prep_csv("BS-Analytics.csv")
