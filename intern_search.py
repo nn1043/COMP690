32e17d09bdf8a9ef41f302f62ec3ae6c888b2320
nn1043 <55171942+nn1043@users.noreply.github.com>
2020-09-20 18:10:23 -0400

    Add files via upload
    added untested error-handling

--- intern_search.py
+++ intern_search.py
@@ -1,9 +1,20 @@
 global intern_list
 intern_list = {}
+intern_list["ID Number"] = [
+    "Last Name", "First Name", "ID Number", "Major", "Phone Number", "Email",
+    "Student LinkedIn URL", "Course Number", "Worked Hours (Logged) ",
+    "Total Credits", "Internship Semester ", "Internship Start Date",
+    "Internship End Date", "Faculty Advisor ","Internship Site",
+    "Mailing Address", "City", "State", '"City, State Zip"',
+    "Internship Position Title", "Site Mentor", "Mentor Position",
+    "Mentor Email", "Mentor Phone ", " Hourly Pay"
+    ]
+
 
 def user_interface():
     menu_list = {
-        "MENU": print_menu, "PREP": prep_csv, "TEST": test_prepped_CSV
+        "MENU": print_menu, "PREP": prep_csv, "TEST": test_prepped_CSV,
+        "SEARCH": search_id
         }
     print("Please choose an operation:")
     print("MENU")
@@ -26,6 +37,8 @@ def user_interface():
 def print_menu():
     print("Prep Test CSV: PREP")
     print("Test Prepped CSV: TEST")
+    print("Search by Student ID: SEARCH")
+    print("Quit Program: QUIT, EXIT")
 
 
 def prep_csv(filename):
@@ -33,6 +46,7 @@ def prep_csv(filename):
     last_name = ""
     first_name = ""
     student_id = ""
+    flag_entry = False
     survey = open(filename, "r", encoding = "ISO-8859-1")
     for line in survey.readlines():
         survey_readable = line.split(",")
@@ -42,12 +56,16 @@ def prep_csv(filename):
             elif survey_readable.index(value) == 1:
                 first_name = value
             elif survey_readable.index(value) == 2: # use student ID for unique
+                for char in value:
+                    if char.isnumeric() == False:
+                        #check_new_sheet(value)
+                        flag_entry = True
                 student_id = value
                 intern_list[student_id] = [last_name, first_name]
             elif value == "":
                 if (check_last == True) & (len(intern_list[student_id]) >= 24):
                     break
-                    #intern_list[student_id].pop() # <- find way to remove last
+                    #intern_list[student_id].pop()   <- find way to remove last
                 else:
                     value = "N/A"
                     intern_list[student_id].append(value)
@@ -55,17 +73,44 @@ def prep_csv(filename):
             else:
                 intern_list[student_id].append(value)
                 check_last = False
+        if flag_entry = True:
+            if check_new_sheet(intern_list[student_id]): # returns True, then..
+                print("whomp whomp") # what now? continue scraping?
+            else:
+                print("Error in loading CSV. Please check formatting.")
+                # assume error-catching fails, throw response to user
     survey.close()
     print("Test CSV loaded.")
 
-# for new csv test initial dict against 'standard' write function for
-# acceptable difference in spelling, or create new column. Maybe check for
-# 'if unique, compare char, then add new column'
+
+"""
+Create a de-facto 'correct' initial column. When index == 2 and char !=
+numeric, pull 'correct' columns and compare. Set acceptable typo level, if
+fails, either kill program (that script) or create new column, possibly give
+user option.
+"""
+
+# This requires the full key/value pair. How do I grab the full key/value
+# without rewriting the entire script? <- I think I got it.
+def check_new_sheet(testing):
+    for item in intern_list["ID Number"]
+        if item in testing: # if default string is found in new dict, pass
+            pass # next need to potentially reindex columns. How?
+        else:
+            return False: # for right now just break
+    return True # every column matches. Will not work with typos at the moment
 
 
 def test_prepped_CSV():
     print(intern_list["913035402"])
 
+
+def search_id():
+    searching_for = input("Enter Student ID: ")
+    if searching_for in intern_list:
+        print(intern_list[searching_for])
+    else:
+        print("Invalid Student ID.")
 
 prep_csv("BS-Analytics.csv")
 user_interface()
