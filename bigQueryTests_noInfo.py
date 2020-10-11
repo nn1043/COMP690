5b9f1f828bf64ef2a7a43ea53739362c2d8e9149
nn1043 <55171942+nn1043@users.noreply.github.com>
2020-10-11 13:37:43 -0400

    Add files via upload

--- bigQueryTests_noInfo.py
+++ bigQueryTests_noInfo.py
@@ -1,20 +1,58 @@
 import os
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
-client = bigquery.Client()
+global query
+query = ""
 
-query = """
+def user_interface():
+    menu_list = {
+        "MENU": print_menu, "PERM": load_permission, "QUERY": run_query
+        }
+    print("Please choose an operation:")
+    print("MENU")
+    run_program = True
+    while run_program is True:
+        user_response = input("Input: ")
+        if user_response.upper() in menu_list:
+            if type(menu_list[user_response.upper()]) is list:
+                for to_do in menu_list[user_response.upper()]:
+                    to_do()
+            else:
+                menu_list[user_response.upper()]()
+        elif user_response.upper() in ["EXIT", "QUIT"]:
+            print("Goodbye.")
+            run_program = False
+        else:
+            print("Not a valid command.")
 
-    SELECT *
-    FROM ***
-    LIMIT 1000
-"""
+
+def print_menu():
+    print("Write a new text file: NEW")
+    print("Load permission file: PERM")
+    print("Run test query: QUERY")
+    print("Exit program: EXIT or QUIT")
 
 
-query_job = client.query(query)  # Make an API request.
+def load_permission():
+    to_open = input("Which permission file would you like to load: ")
+    to_open = to_open + ".txt"
+    file = open(to_open, "r")
+    global query
+    query  = file.read()
+    print(query)
 
-print("The query data:")
-for row in query_job:
-    # Row values can be accessed by field name or index.
-    print("First Name = {}, Last Name = {}".format(row[1], row[0]))
+
+def run_query():
+    client = bigquery.Client()
+    print(query)
+    if query == "":
+        print("You do not have permission to do this.")
+    else:
+        query_job = client.query(query)
+        # User interactivity would begin below this line.
+        print("Students")
+        for row in query_job:
+            print("{}, {}".format(row[0], row[1]))
+
+
+user_interface()
